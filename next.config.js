52c624081bca29e46bf41bc975c0d2a8a3bda0ee
sasha982025 <sasha982025@gmail.com>
2022-10-27 01:39:44 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -14,18 +14,4 @@ module.exports = {
   },
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build.
-    // With this you ship way less javascript to the client I mean almost half.
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-        'react/jsx-runtime': 'preact/jsx-runtime'
-      });
-    }
-
-    return config;
-  }
 }
