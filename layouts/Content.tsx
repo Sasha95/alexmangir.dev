bc7a422868948eeaa62182f76c89c664db660719
sasha982025 <sasha982025@gmail.com>
2022-12-19 00:13:54 +0200

    test

--- layouts/Content.tsx
+++ layouts/Content.tsx
@@ -17,7 +17,7 @@ export const Content = () => {
         </h4>
       </div>
       <Image
-        src={siteMetadata.wisent}
+        src={siteMetadata.footerImage}
         alt={"main image"}
         sizes="100vw"
         width="0"
