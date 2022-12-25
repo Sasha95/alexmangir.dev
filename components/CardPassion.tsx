40e26cc59ea201d2d0ccb9d6f4141b3d8fbe499a
sasha982025 <sasha982025@gmail.com>
2022-12-25 03:47:14 +0200

    refactor

--- components/CardPassion.tsx
+++ components/CardPassion.tsx
@@ -10,16 +10,18 @@ type Props = {
 export const CardPassion = ({ work, description, image, alt }: Props) => {
   return (
     <div className="bg-white shadow-3xl rounded-xl w-full xl:min-w-[346px] xl:max-h-[289px] py-7 md:py-4 lg:pt-7 xl:py-5 lg:pb-5 md:px-3 px-6 flex flex-col items-center">
-      <Image
-        alt={alt}
-        src={image}
-        sizes="100vw"
-        width="0"
-        height="0"
-        className={
-          "px-16 md:px-12 lg:px-0 max-xs:p-0 relative w-full h-auto xl:max-w-[225px] xl:max-h-[159px] lg:max-w-[152px] lg:max-h-[104px]"
-        }
-      />
+      <div className="relative">
+        <Image
+          alt={alt}
+          src={image}
+          sizes="100vw"
+          width="0"
+          height="0"
+          className={
+            "px-16 md:px-12 lg:px-0 max-xs:p-0 relative w-full h-auto xl:max-w-[225px] xl:max-h-[159px] lg:max-w-[152px] lg:max-h-[104px]"
+          }
+        />
+      </div>
       <div className="flex mt-4">
         <p className="font-semibold xl:text-2xl xl:leading-9 lg:text-base lg:leading-6 md:text-sm md:leading-5 text-2xl leading-8">
           {work} &nbsp;
