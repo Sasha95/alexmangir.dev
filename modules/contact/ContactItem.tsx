cc6b1ea79b17020f202f01ef69bf9d267eb65022
sasha982025 <sasha982025@gmail.com>
2022-12-26 16:44:57 +0200

    refactor

--- modules/contact/ContactItem.tsx
+++ modules/contact/ContactItem.tsx
@@ -12,7 +12,7 @@ export const ContactItem = ({ title, src, href }: Props) => (
     className="flex flex-col gap-y-2.5 md:gap-y-4 lg:gap-y-6 xl:gap-y-7 items-center hover:text-blue"
     rel="noreferrer"
   >
-    <div className="relative max-h-[160px] max-w-[166px]">
+    <div className="relative">
       <Image alt={title} src={src} fill className="relative" />
     </div>
     <h2 className="font-medium letterSpacing-normal leading-5 contactText">
